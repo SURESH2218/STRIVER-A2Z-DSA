d2da4e562e22a0fb17e2f0f00ea31cf5b43bc537
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-02-09 14:05:50 +0530

    p added

--- patterns/pattern8.cpp
+++ patterns/pattern8.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 
 void pattern7(int num){
+     for(int i=0;i<num;i++){
+        for(int j=0;j<num-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"x";
+        }
+    cout<<endl;
+    }
     for(int i=0;i<num;i++){
         for(int j=0;j<i;j++){
             cout<<" ";
