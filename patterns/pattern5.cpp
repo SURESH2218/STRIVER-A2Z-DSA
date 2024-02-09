b2e5bfe22e7f7cfc21ffb04ba1ef1e6ca5fddeac
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-02-09 13:16:04 +0530

    patterns added

---
+++ patterns/pattern5.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void pattern5(int num){
+    for(int i=num;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int num;
+    cin>>num;
+    pattern5(num);
+}
